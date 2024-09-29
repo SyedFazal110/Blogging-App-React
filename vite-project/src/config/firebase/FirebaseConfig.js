4e83c022ae706de104e463c99f9d69a9d50d2e84
SyedFazal110 <syedfazal808@gmail.com>
2024-09-29 14:53:51 +0500

    Firebase Installed

---
+++ vite-project/src/config/firebase/FirebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCiv_McPqdxH7-pFNcMSne6UIiLi23TiAI",
+  authDomain: "blogging-app-f4bda.firebaseapp.com",
+  projectId: "blogging-app-f4bda",
+  storageBucket: "blogging-app-f4bda.appspot.com",
+  messagingSenderId: "1041200374283",
+  appId: "1:1041200374283:web:0f3946ecb0e8fb07445d0e",
+  measurementId: "G-S1YPRC7QBX"
+};
+
+
+export const app = initializeApp(firebaseConfig);
