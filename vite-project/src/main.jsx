be68a27942c0074c7d79b1305d03ab5c40b7af8a
SyedFazal110 <syedfazal808@gmail.com>
2024-09-29 15:03:08 +0500

    tailwind & components installed

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,44 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+import Profile from './pages/Profile'
+import ProtectedRoutes from './components/ProtectedRoutes'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <Home />
+      },
+      {
+        path: 'login',
+        element: <Login />
+      },
+      {
+        path: 'signup',
+        element: <Signup />
+      },
+      {
+        path: 'dashboard',
+        element: <ProtectedRoutes component={<Dashboard />}/>
+      },
+      {
+        path: 'profile',
+        element: <ProtectedRoutes component={<Profile />}/>
+      },
+    ]
+  },
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router}></RouterProvider>
 )
